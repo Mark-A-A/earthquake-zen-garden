66923fb5f8133a40250a2770156d12299971e301
Mark-A-A <markawai01@gmail.com>
2021-08-06 19:26:55 -0400

    add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -44,10 +44,11 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
 
-      // {
-      //   test: /\.(png|j?g|svg|gif)?$/,
-      //   use: 'file-loader'
-      // }
+      {
+        test: /\.(png|j?g|svg|gif|ico)?$/,
+        use: 'file-loader',
+        type: 'javascript/auto'
+      }
     ],
   },
   plugins: [
@@ -56,6 +57,7 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: path.resolve(__dirname, 'public/index.html'),
       filename: 'index.html',
+      favicon: path.resolve(__dirname, 'public/favicon.ico'),
     }),
   ],
   optimization: {
